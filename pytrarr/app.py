147e0e57b1f2a136d06608dc8c78443cd0d0b1b7
Fabio Souza <fabiohcsouza@outlook.com.br>
2023-10-06 11:52:21 -0300

    modified:   pytrarr/__pycache__/templates.cpython-311.pyc 	modified:   pytrarr/app.py 	modified:   pytrarr/templates.py

--- pytrarr/app.py
+++ pytrarr/app.py
@@ -3,9 +3,7 @@ from nicegui.events import KeyEventArguments
 
 
 from modules import TMDB
-from templates import (
-    add_result_item
-)
+from templates import *
 
 
 
@@ -31,8 +29,54 @@ async def search_submit(slot: ui.grid, query: str):
 
 
 dark = ui.dark_mode(value=True)
-    
+
+def handle_key(e: KeyEventArguments):
+    if e.key == 'f' and not e.action.repeat:
+        if e.action.keyup:
+            ui.notify('f was just released')
+        elif e.action.keydown:
+            ui.notify('f was just pressed')
+
+keyboard = ui.keyboard(on_key=handle_key)
+
 @ui.page('/')
+def page():
+    Header()
+    with Body() as body:
+        with Title('Welcome to PYTRARR') as title:
+            title.classes('text-center w-full border-2 border-gray-300')
+        
+        with Section() as section_search:
+            section_search.classes('items-center justify-center flex flex-row w-full border-2 border-gray-300')
+            SubTitle('Search')
+            input_search = Input(
+                label='Search',
+                placeholder='Search...'
+            )
+            input_search.icon()
+            input_search.callback(ui.notify)
+
+        with ui.dialog() as dialog, ui.card() as card:
+            card.classes('w-full rounded-lg border bg-gray-200')
+            with ui.card_section() as title:
+                title.classes('w-full rounded-lg border bg-gray-300')
+                ui.label('Hello world!')
+
+            with ui.card_section() as search:
+                search.classes('w-full rounded-lg border bg-gray-300')
+                input_search = Input(
+                    label='Search',
+                    placeholder='Search...'
+                )
+                input_search.icon()
+                input_search.callback(ui.notify)
+
+            with ui.card_actions() as actions:
+                actions.classes('w-full rounded-lg border bg-gray-300')
+                ui.button('Close', on_click=dialog.close)
+
+        ui.button('Open a dialog', on_click=dialog.open)
+        ui.checkbox('Track key events').bind_value_to(keyboard, 'active')
 @ui.page('/home')
 async def page_home():
     ##############
--- pytrarr/templates.py
+++ pytrarr/templates.py
@@ -1,5 +1,56 @@
+from typing import Any
+
 from nicegui import ui
 
+
+theme = {
+    'lightThemeColors': {
+        'bg': 'bg-white',
+        'fg': 'text-gray-800',
+        'text': 'text-gray-700',
+        'border': 'border-gray-300',
+        'hover': 'hover:bg-gray-100',
+        'focus': 'focus:bg-gray-100',
+        'active': 'active:bg-gray-200'
+        },
+    'darkThemeColors': {
+        'bg': 'bg-gray-900',
+        'fg': 'text-white',
+        'text': 'text-gray-400',
+        'border': 'border-gray-700',
+        'hover': 'hover:bg-gray-800',
+        'focus': 'focus:bg-gray-800',
+        'active': 'active:bg-gray-700'
+        }
+    }
+
+# light_theme_colors = {
+#     'primary': '#2196F3',
+#     'secondary': '#FFC107',
+#     'accent': '#9C27B0',
+#     'dark': '#333333',
+#     'light': '#FFFFFF',
+#     'text': '#333333',
+#     'secondary_text': '#666666',
+#     'border': '#E0E0E0',
+#     'hover': '#EEEEEE',
+#     'focus': '#E0E0E0',
+#     'active': '#CCCCCC'
+# }
+
+# dark_theme_colors = {
+#     'primary': '#2196F3',
+#     'secondary': '#FFC107',
+#     'accent': '#9C27B0',
+#     'dark': '#121212',
+#     'light': '#333333',
+#     'text': '#FFFFFF',
+#     'secondary_text': '#CCCCCC',
+#     'border': '#333333',
+#     'hover': '#333333',
+#     'focus': '#666666',
+#     'active': '#888888'
+# }
 
 def add_result_item(info: dict):
     with ui.card().classes('p-4'):
@@ -23,3 +74,67 @@ def add_result_item(info: dict):
             with ui.row().classes('flex items-center flex-wrap'):
                 ui.link('Learn more').classes('text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0')
                 ui.icon('arrow right alt')
+                
+
+class Body(ui.column):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.classes('items-center justify-center w-full')
+        self.props('')
+
+
+class Card(ui.card):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.classes('')
+        self.props('')
+
+class Input(ui.input):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.classes('')
+        self.props('outlined')
+        self.call_back: Any = None
+    
+    def icon(self, 
+        icon: str = 'search', 
+        size: str = '12'
+        ):
+        with self:
+            ui.icon(icon, size=size)\
+                .classes(
+                    'py-4 text-gray-400 text-2xl cursor-pointer'
+                )\
+                .on('click', self.callback)
+
+    def callback(self, callback: Any):
+        self.call_back = callback if self.call_back is None else self.call_back
+        self.on('Enter', self.call_back(self.value))
+class Header(ui.header):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.classes('')
+        self.props('')
+
+
+class Section(ui.row):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.classes('')
+        self.props('')
+
+class Title(ui.label):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.classes('text-3xl font-mono-bold')
+        self.props('')
+
+class SubTitle(ui.label):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.classes('text-2xl font-mono-semibold')
+        self.props('')
+
+class Filters(ui.expansion):
+    pass
+
